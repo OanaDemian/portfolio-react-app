799bdc243bdf6d3dffabf9bacd9a3f28a2b18cce
Oana <oana.demian@gmail.com>
2023-04-17 16:08:49 +0100

    creates about component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import About from './components/About.jsx';
+import Home from './components/Home.jsx';
+import Navbar from './components/Navbar.jsx';
+import Skills from './components/Skills.jsx';
+
+function App() {
+  return (
+    <div >
+      <Navbar />
+      <Home />
+      <About />
+    </div>
+  );
+}
+
+export default App;
